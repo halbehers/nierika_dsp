c7cb8e4e0cd2f31a4eb98fa768262ca04acef8ef
halbehers <sebastien.halbeher@free.fr>
2025-04-08 13:53:52 +0200

    Juce module init

---
+++ nierikadsp.cpp
@@ -0,0 +1,46 @@
+#ifdef NIERIKA_MODULES_H_INCLUDED
+ /* When you add this cpp file to your project, you mustn't include it in a file where you've
+    already included any other headers - just put it inside a file on its own, possibly with your config
+    flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
+    header files that the compiler may be using.
+ */
+ #error "Incorrect use of JUCE cpp file"
+#endif
+
+#include "nierikadsp.h"
+
+// DSP
+#include "source/dsp/CircularAudioBuffer.cpp"
+#include "source/dsp/Timing.cpp"
+#include "source/dsp/delay/SimpleDelay.cpp"
+#include "source/dsp/filter/Filter.cpp"
+#include "source/dsp/filter/HighPassFilter.cpp"
+#include "source/dsp/filter/LowPassFilter.cpp"
+#include "source/dsp/sequencer/FXSequencer.cpp"
+
+// GUI
+#include "source/gui/EmbeddedFonts.cpp"
+#include "source/gui/Formatter.cpp"
+#include "source/gui/Helpers.cpp"
+#include "source/gui/element/BoxToggle.cpp"
+#include "source/gui/element/ComboBox.cpp"
+#include "source/gui/element/Dial.cpp"
+#include "source/gui/element/FrequencyDial.cpp"
+#include "source/gui/element/HorizontalSlider.cpp"
+#include "source/gui/element/IncrementalButton.cpp"
+#include "source/gui/element/IncrementalButtons.cpp"
+#include "source/gui/element/Level.cpp"
+#include "source/gui/element/PercentageDial.cpp"
+#include "source/gui/element/Slider.cpp"
+#include "source/gui/element/Stars.cpp"
+#include "source/gui/element/SVGButton.cpp"
+#include "source/gui/element/SVGToggle.cpp"
+#include "source/gui/element/TimeInMsDial.cpp"
+#include "source/gui/element/TimingComboBox.cpp"
+#include "source/gui/element/TimingDial.cpp"
+#include "source/gui/laf/BoxToggle.cpp"
+#include "source/gui/laf/ComboBox.cpp"
+#include "source/gui/laf/Dial.cpp"
+#include "source/gui/laf/IncrementalButtons.cpp"
+#include "source/gui/laf/SVGButton.cpp"
+#include "source/gui/laf/SVGToggle.cpp"
--- nierikadsp.h
+++ nierikadsp.h
@@ -1,6 +1,32 @@
 #pragma once
 #define NIERIKA_MODULES_H_INCLUDED
 
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.md file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:                 nierikadsp
+  vendor:             nierika
+  version:            1.0.0
+  name:               Nierka's DSP & GUI classes based on JUCE
+  description:        Classes for GUI creation and DSP processing.
+  website:            http://www.juce.com/juce
+  license:            MIT
+  minimumCppStandard: 17
+
+  dependencies:       juce_core
+  OSXFrameworks:      Accelerate
+  iOSFrameworks:      Accelerate
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
 // DSP
 #include "includes/dsp/Channel.h"
 #include "includes/dsp/CircularAudioBuffer.h"
